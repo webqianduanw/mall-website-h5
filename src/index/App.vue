7d4b5e063cf42c95e34da113399da7ada5afd853
simplePoint <971977541@qq.com>
2021-06-21 11:51:32 +0800

    fix：代码提交

--- src/index/App.vue
+++ src/index/App.vue
@@ -4,7 +4,7 @@
       <div class="header-box clearfix">
         <div class="fleft header-box-left" @click="jumpClick(1)">
           <div class="img fleft"><img src="../assets/img/index/logo.png" alt=""></div>
-          <div class="app-name fleft">缤惠购商城</div>
+          <div class="app-name fleft">核桃返利</div>
         </div>
         <div class="fright header-box-right">
           <div class="fleft download positionR">
@@ -17,34 +17,22 @@
       <div class="header-box clearfix">
         <div class="fleft header-box-left">
           <div class="img fleft"><img src="../assets/img/index/logo.png" alt=""></div>
-          <div class="app-name fleft">缤惠购商城</div>
+          <div class="app-name fleft">核桃返利</div>
         </div>
         <div class="fright header-box-right">
           <div class="fleft download" @mouseenter="mouseover" @mouseleave="mouseLeave" @click="goTop">
             <!-- <a href="javascript:;">下载App</a> -->
             <img class="ewmcode positionA" ref="ewmcode" style="display:none;" src="../assets/img/ewmcode.png" alt="">
           </div>
-          <!-- <div class="fleft about">
-            招商电话：4000225592
-          </div> -->
+          <div class="fleft about">
+            购物返利·一站式商城·全场包邮
+          </div>
         </div>
       </div>
     </div>
     <div class="content w">
       <div class="content1">
-        <img src="../assets/img/index/index1_bg1.png" alt="">
-      </div>
-      <div class="content2 positionR">
-        <img class="content2_txt1 positionA" src="../assets/img/index/index1_txt1.png" alt="">
-        <div class="content2_txt2 positionA">
-          海量电器
-        </div>
-        <div class="content2_txt3 positionA">
-          平台商品涵盖厨房家电、个护家电、3C数码等平台精选好货。
-        </div>
-        <div class="content2_txt4 positionA">
-          <p>TPlatform products include kitchen appliances, personal care appliances, 3C digital and other platform selected good products.</p>
-        </div>
+        <img src="../assets/img/index/index1_bg1.jpg" alt="">
       </div>
       <div class="content3">
         <div class="swiper-container">
@@ -55,128 +43,56 @@
                 <div class="swiper-slide">
                   <img src="../assets/img/index/swiper2.jpg" alt="">
                 </div>
+                 <div class="swiper-slide">
+                  <img src="../assets/img/index/swiper3.jpg" alt="">
+                </div>
+                 <div class="swiper-slide">
+                  <img src="../assets/img/index/swiper4.jpg" alt="">
+                </div>
+                 <div class="swiper-slide">
+                  <img src="../assets/img/index/swiper5.jpg" alt="">
+                </div>
             </div>
             <div class="swiper-pagination"></div>
-        </div>
-      </div>
-      <div class="content4 content2 positionR">
-        <img class="content2_txt1 positionA" src="../assets/img/index/index1_txt2.png" alt="">
-        <div class="content2_txt2 positionA">
-          严选好货
-        </div>
-        <div class="content2_txt3 positionA">
-          每一道工序我们都坚持多思考30%的定制细节，为消费者提供高品质生活整体解决方案
-        </div>
-        <div class="content2_txt4 positionA">
-          <p> Each process we insist on thinking more than 30% of the customized details, </p>
-          <p>to provide consumers with a high-quality life overall solution</p>
-        </div>
-      </div>
-      <div class="content5">
-        <div class="content5_cont clearfix">
-          <div class="content5_cont_left fleft">
-            <div class="content5_cont_left_txt1">
-              严选好物
-            </div>
-            <div class="content5_cont_left_txt2">
-              平台会认证挑选每一件上架商品
-            </div>
-             <div class="content5_cont_left_txt3">
-               <span>从</span>源头、质量、服务
-               <span>等10多个维度对供货商进行考核.</span>
-            </div>
-            <div class="content5_cont_left_txt4">
-              充分保障消费者的权益、让您买的省心、买地安心。
-            </div>
-          </div>
-          <div class="content5_cont_right fleft clearfix">
-            <img class="fleft" src="../assets/img/index/content5_txt1.png" alt="">
-            <img class="fleft" src="../assets/img/index/content5_txt2.png" alt="">
-          </div>
-        </div>
-      </div>
-      <div class="content6 content2 positionR">
-        <img class="content2_txt1 positionA" src="../assets/img/index/index1_txt3.png" alt="">
-        <div class="content2_txt2 positionA">
-          巨额优惠
-        </div>
-        <div class="content2_txt3 positionA">
-          第一 时间为您推送第一手的优惠资讯
-        </div>
-        <div class="content2_txt4 positionA">
-          <p>The first time for you to push the first-hand information</p>
         </div>
       </div>
       <div class="content7">
         <img src="../assets/img/index/content7.jpg" alt="">
       </div>
-      <div class="content8 content2 positionR">
-        <img class="content2_txt1 positionA" src="../assets/img/index/index1_txt4.png" alt="">
-        <div class="content2_txt2 positionA">
-          专业服务
-        </div>
-        <div class="content2_txt3 positionA">
-          您的需求是我们的工作目标，我们提供符合您真实的需求。
-        </div>
-        <div class="content2_txt4 positionA">
-          <p>Your requirements are our work objectives, we provide to meet your real needs.</p>
-        </div>
-      </div>
       <div class="content9">
-        <div class="content9_cont clearfix">
-          <div class="content9_cont_left fleft">
-            <img src="../assets/img/index/content9_txt1.png" alt="">
-          </div>
-          <div class="content9_cont_right fleft clearfix">
-            <div class="content9_cont_right_txt1 fleft">
-              <p>专业</p>
-              <p>服务</p>
+        <div class="content9_cont">
+          <div class="content9_cont_left">
+            <div class="content9_cont_left_txt1">
+              公司：郴州市昌瑞贸易有限公司
             </div>
-            <div class="content9_cont_right_txt2 fleft"></div>
-            <div class="content9_cont_right_txt3 fleft">
-              <div class="content9_cont_right_txt3_child1">专业的客户服务团队，为您服务</div>
-              <div class="content9_cont_right_txt3_child2"></div>
-              <div class="content9_cont_right_txt3_child3">
-                <p>订单支付后急速为您发货，全场商品免费包邮</p>
-                <p>售后无忧，大部分商品支持7天无理由退换，专业的服务只为给您更优质的体验。</p>
-              </div>
+            <div class="content9_cont_left_txt2">
+              地址：湖南省郴州市嘉禾县珠泉镇中伟神农步行街4栋2单元501室
+            </div>
+            <div class="content9_cont_left_txt3">
+              邮箱：q13396009274@163.com
             </div>
           </div>
+          <div class="content9_cont_right">
+            <div class="content9_cont_right_txt1">
+              客服联系方式(APP内在线咨询)
+            </div>
+            <div class="content9_cont_right_txt2">
+               客服工作时间
+            </div>
+            <div class="content9_cont_right_txt3">
+               工作日：9：00-22：00
+            </div>
+            <div class="content9_cont_right_txt4">
+               周末及节假日：9：00-21：00
+            </div>
+          </div>
         </div>
       </div>
-      <div class="content10 content2 positionR">
-        <img class="content2_txt1 positionA" src="../assets/img/index/index1_txt5.png" alt="">
-        <div class="content2_txt2 positionA">
-          极致体验
-        </div>
-        <div class="content2_txt3 positionA">
-          极致的购物体验，让您开心购物
-        </div>
-        <div class="content2_txt4 positionA">
-          <p>The ultimate shopping experience, let you happy shopping</p>
-        </div>
-      </div>
-      <div class="content11">
-        <img src="../assets/img/index/content11.png" alt="">
-      </div>
-      <div class="content12 content2 positionR">
-        <img class="content2_txt1 positionA" src="../assets/img/index/index1_txt6.png" alt="">
-        <div class="content2_txt2 positionA">
-          关于我们
-        </div>
-        <div class="content2_txt3 positionA">
-          专业家电商城
-        </div>
-        <div class="content2_txt4 positionA">
-          <p>Professional home appliance mall</p>
-        </div>
-      </div>
-
     </div>
     <div class="footer">
       <div class="footer_cont">
-        <a href="http://beian.miit.gov.cn/?spm=a2c4g.11186623.7y9jhqsfz.111.26b47243MaN2zM" target="_blank" data-spm-anchor-id="a2c4g.11186623.7y9jhqsfz.111">杭ICP备 34687532号-1</a>
-              http://www.af.yunsharemall.com/ @ 2021 缤惠购商城版权所有
+        <a href="http://beian.miit.gov.cn/?spm=a2c4g.11186623.7y9jhqsfz.111.26b47243MaN2zM" target="_blank" data-spm-anchor-id="a2c4g.11186623.7y9jhqsfz.111">湘ICP备2021004451号</a>
+              http://www.walnutmall.com/ @ 2021 核桃返利版权所有
       </div>
     </div>
   </div>
@@ -254,11 +170,12 @@ body{
   min-height: 600px;
 }
 .header{
-  position: fixed;
-  z-index: 10;
-  top: 0;
+  // position: fixed;
+  // z-index: 10;
+  // top: 0;
   background:#ffffff;
   width: 100%;
+  height: 100px;
 }
 .header-box{
   width: 1200px;
@@ -280,7 +197,7 @@ body{
       line-height: 100px;
       font-size:18px;
       font-weight:bold;
-      color:#DC3731;
+      color:#FF7002;
       margin-left: 15px;
     }
 
@@ -440,69 +357,59 @@ body{
   }
 }
 .content7{
-  margin: 112px 0 146px 0;
   img{
     width: 100%;
     height: auto;
   }
 }
 .content9{
-  height: 486px;
+  height: 282px;
   width: 100%;
   margin: 0 auto;
+  background: #626262;
   .content9_cont{
     width: 1200px;
     margin: 0 auto;
+    clear: both;
     .content9_cont_left{
-      margin-right: 45px;
-      margin-top: 26px;
-      img{
-        width: 350px;
-        height: 350px;
+      float: left;
+      .content9_cont_left_txt1{
+        margin-top: 90px;
+        font-size: 18px;
+        color: #FFFFFF;
+      }
+      .content9_cont_left_txt2{
+        margin-top: 14px;
+        font-size: 18px;
+        color: #FFFFFF;
+      }
+      .content9_cont_left_txt3{
+        margin-top: 14px;
+        font-size: 18px;
+        color: #FFFFFF;
       }
     }
     .content9_cont_right{
+      float: right;
       .content9_cont_right_txt1{
-        margin-top: 126px;
-        p{
-          font-size:42px;
-          font-family:Source Han Sans CN;
-          font-weight:bold;
-          color:rgba(220,55,49,1);
-        }
-        margin-right: 30px;
+        margin-top: 90px;
+        font-size: 18px;
+        color: #FFFFFF;
       }
       .content9_cont_right_txt2{
-        margin-top: 124px;
-        width:4px;
-        height:119px;
-        background:rgba(228,228,228,1);
-        margin-right: 40px;
+        margin-top: 10px;
+        font-size: 18px;
+        color: #FFFFFF;
       }
       .content9_cont_right_txt3{
-        margin-top: 115px;
-        .content9_cont_right_txt3_child1{
-          font-size:24px;
-          font-family:Adobe Heiti Std;
-          font-weight:normal;
-          color:rgba(51,51,51,1);
-          line-height: 30px;
-        }
-        .content9_cont_right_txt3_child2{
-          width:340px;
-          height:4px;
-          background:rgba(220,55,49,1);
-          margin-bottom: 42px;
-        }
-        .content9_cont_right_txt3_child3{
-          width:582px;
-          height:41px;
-          font-size:16px;
-          font-family:Microsoft YaHei;
-          font-weight:400;
-          color:rgba(153,153,153,1);
-          line-height:24px;
-        }
+        margin-top: 10px;
+        font-size: 18px;
+        color: #FFFFFF;
+      }
+      .content9_cont_right_txt4{
+        margin-top: 10px;
+        font-size: 18px;
+        color: #FFFFFF;
       }
     }
   }
